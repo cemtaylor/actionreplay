790aad687e02dad49c5d0dadbce16803304c71d3
Chris Taylor <chris@cemt.co.uk>
2019-05-14 12:46:01 +0100

    * Froze requirements.txt * Moved monitoring actions into recorder class * Added logging * Added mouse scrolling replay * Tidied up mouse move replay

--- actionreplay_app/actionreplay.py
+++ actionreplay_app/actionreplay.py
@@ -1,63 +1,52 @@
 #!/usr/bin/env python3
 import time
+import logging
 from timeit import default_timer as timer
 from pynput import mouse, keyboard
 
 
-def log_move(x, y):
-    action_log.append(x, y)
-
-
-def log_click(x, y, button, pressed):
-    action_log.append(x, y, button=button, pressed=pressed)
-
-
-def log_scroll(x, y, dx, dy):
-    action_log.append(x=x, y=y, scrollX=dx, scrollY=dy)
-
-
-def log_key_down(key):
-    action_log.append(key=key, pressed=True)
-
-
-def log_key_up(key):
-    action_log.append(key=key, pressed=False)
-
-
-def replay(actions):
+def ActionReplay(actions):
+    time.sleep(1)
+    logger.info('Replay started')
     for action in actions:
         time.sleep(action['time'])
         if action['key']:
             if action['pressed']:
                 keyboard_controller.press(action['key'])
-                print(f"Key: {action['key']} Pressed")
+                logger.debug("Key %s Pressed", action['key'])
             else:
                 keyboard_controller.release(action['key'])
-                print(f"Key: {action['key']} Released")
+                logger.debug("Key %s Released", action['key'])
         else:
-            mouse_controller.position = action['pos']
-            print(f"Mouse moved to: {action['pos']}")
+            if mouse_controller.position != action['pos']:
+                mouse_controller.position = action['pos']
+                logger.debug("Mouse moved to %s", action['pos'])
+            if action['scrollX'] or action['scrollY']:
+                mouse_controller.scroll(action['scrollX'], action['scrollY'])
+                logger.debug(
+                    f"Scrolled {action['scrollX'],action['scrollY']}")
             if action['button']:
-                print(f"Mouse button: {action['button']}")
                 if action['pressed']:
                     mouse_controller.press(action['button'])
-                    print(f"Mouse button: {action['button']} Pressed")
+                    logger.debug("Mouse button %s Pressed", action['button'])
                 else:
                     mouse_controller.release(action['button'])
-                    print(f"Mouse button: {action['button']} Released")
+                    logger.debug(
+                        "Mouse button %s Released", action['button'])
 
 
-class ActionLog(object):
+class ActionRecorder(object):
     def __init__(self):
         self.action_log = []
         self.last_action = timer()
-        self.recording = True
+        self.recording = False
+        logger.info('Action Recorder initialized')
 
-    def append(self, x=0, y=0, button=None, pressed=False, scrollX=0, scrollY=0, key=None):
+    def add_action(self, x=0, y=0, button=None, pressed=False, scrollX=0, scrollY=0, key=None):
         if self.recording == False:
             return
         if key == keyboard.Key.esc:
-            print("User entered escape - ending recording")
+            logger.info('Escape key detected - Ending recording')
             self.recording = False
             return
         current_time = timer()
@@ -74,33 +63,77 @@ class ActionLog(object):
         }
         self.action_log.append(action)
 
+    def log_move(self, x, y):
+        self.add_action(x, y)
+
+    def log_click(self, x, y, button, pressed):
+        self.add_action(x, y, button=button, pressed=pressed)
+
+    def log_scroll(self, x, y, dx, dy):
+        self.add_action(x=x, y=y, scrollX=dx, scrollY=dy)
+
+    def log_key_down(self, key):
+        self.add_action(key=key, pressed=True)
+
+    def log_key_up(self, key):
+        self.add_action(key=key, pressed=False)
+
+    def start(self):
+        self.recording = True
+        logger.info('Recording started')
+
+    def stop(self):
+        self.recording = False
+        logger.info('Recording stopped')
+
 
 def main():
     try:
-        while action_log.recording:
-            pass
-        print("REPLAYING ACTIONS")
-        replay(action_log.action_log)
+        while Recorder.recording:
+            time.sleep(1)
+        ActionReplay(Recorder.action_log)
     except KeyboardInterrupt:
-        pass
+        logger.warning("Keyboard interrupt - application exiting")
 
 
 if __name__ == '__main__':
-    action_log = ActionLog()
+    # Configure logging
+    logger = logging.getLogger('actionreplay')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        fmt='%(asctime)s,%(levelname)s,%(message)s',
+        datefmt='%Y-%m-%d,%H:%M:%S%z'
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    logfile_handler = logging.FileHandler(filename="actionreplay.log")
+    logfile_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logfile_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.addHandler(logfile_handler)
+    ###
+    logger.info('Application started')
+    # Create recorder instance
+    Recorder = ActionRecorder()
+    # Start listeners & create controllers for kb/m
     mouse_listener = mouse.Listener(
-        on_move=log_move,
-        on_click=log_click,
-        on_scroll=log_scroll
+        on_move=Recorder.log_move,
+        on_click=Recorder.log_click,
+        on_scroll=Recorder.log_scroll
     )
     mouse_listener.start()
+    logger.info('Mouse listener started')
     mouse_controller = mouse.Controller()
 
     keyboard_listener = keyboard.Listener(
-        on_press=log_key_down,
-        on_release=log_key_up
+        on_press=Recorder.log_key_down,
+        on_release=Recorder.log_key_up
     )
     keyboard_listener.start()
+    logger.info('Keyboard listener started')
     keyboard_controller = keyboard.Controller()
-    print("Recording Active")
+    # Start the recorder
+    Recorder.start()
     main()
-    print("Ended")
+    logger.info('Application closed')
